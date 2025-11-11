ccf8ffa2fd52a5fe62da2586b4dd84a6f4ac59a3
cosme <cportuon@gmail.com>
2025-11-11 18:37:15 +0100

    Tarea condicionales 1

--- TareaJava/src/main/java/com/mycompany/tareajava/TareaCondicionales.java
+++ TareaJava/src/main/java/com/mycompany/tareajava/TareaCondicionales.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.tareajava;
 
-/**
- *
- * @author cosisi
- */
+package com.mycompany.tareajava;
+import java.util.Scanner;
+
 public class TareaCondicionales {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número:");
+        int num1 = sc.nextInt();
+        
+        System.out.println("Introduce otro número:");
+        int num2 = sc.nextInt();
+        
+        System.out.println("Introduce un último número:");
+        int num3 = sc.nextInt();
+        
+        int mayor;
+        int menor;
+        
+        if ((num1 >= num2) && (num1 >= num3)){
+            mayor = num1;
+        }
+        else if ((num2 >= num3) && (num2 >= num1)){
+            mayor = num2;
+        }
+        else mayor = num3;
+        
+        if ((num1 <= num2) && (num1 <= num3)){
+            menor = num1;
+        }
+        else if ((num2 <= num3) && (num2 <= num1)){
+            menor = num2;
+        }
+        else menor = num3;
+        
+        System.out.println("EL número mayor es:" + mayor);
+        System.out.println("EL número menor es:" + menor);
+
+
+    }
 }

36157dfeb19d437e56656cad47916ce752729b51
cosme <cportuon@gmail.com>
2025-11-11 21:41:41 +0100

    Ejercicios de condicionales II

---
+++ TareaJava/src/main/java/com/mycompany/tareajava/TareaIfSimple.java
@@ -0,0 +1,21 @@
+package com.mycompany.tareajava;
+import java.util.Scanner;
+public class TareaIfSimple {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un número:");
+        int num = scanner.nextInt();
+        System.out.println("Introduce otro número:");
+        int num2 = scanner.nextInt();
+        int mayor = Math.max(num, num2);
+        System.out.println("Este es el número mayor: " + mayor);
+        
+        System.out.println("Introduce el primer número:");
+        int n1 = scanner.nextInt();
+        System.out.println("Introduce el segundo número:");
+        int n2 = scanner.nextInt();    
+        System.out.println("Introduce el tercer número:");
+        int n3 = scanner.nextInt();
+        int mayor = Math.
+    }
+}

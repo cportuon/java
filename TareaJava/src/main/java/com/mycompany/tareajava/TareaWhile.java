222a90d5c1d35466ed5c381d356d778cc96f9ef0
cosme <cportuon@gmail.com>
2025-11-12 20:44:03 +0100

    TareaWhile finalizada

---
+++ TareaJava/src/main/java/com/mycompany/tareajava/TareaWhile.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.tareajava;
+import java.util.Scanner;
+
+public class TareaWhile {
+    
+    public static void main(String[]args){
+        
+        Scanner sc = new Scanner(System.in);
+        int valor  = 0;
+        int suma = 0;
+        
+        while(valor >= 0){
+            
+            System.out.println("________________________________________\n");
+            System.out.println("Introduce un número entero positivo:");
+            valor  = sc.nextInt();
+            suma = suma + valor;
+            System.out.println("Suma de los valores: " + suma);
+        }
+    }
+}

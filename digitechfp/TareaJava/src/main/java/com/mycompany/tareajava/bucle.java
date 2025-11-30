74dab968e0175de928c32996d575def8ba25da91
cosme <cportuon@gmail.com>
2025-11-30 10:34:48 +0100

    Correccion tarea bucle

--- digitechfp/TareaJava/src/main/java/com/mycompany/tareajava/bucle.java
+++ digitechfp/TareaJava/src/main/java/com/mycompany/tareajava/bucle.java
@@ -2,19 +2,20 @@
 package com.mycompany.tareajava;
 import java.util.Scanner;
 
-public class pruebas {
+public class bucle{
 
 
     public static void main(String args[]) {
         
-        /*for (int i = 1; i <= 100; i++){
+        for (int i = 1; i <= 100; i++){
             System.out.println(i);
-        }*/
-        /*int i=1;
+        }
+             
+        int i=1;
         while(i<=100){
             System.out.println(i);
             i++;
-        }*/
+        }
        
         Scanner sc = new Scanner(System.in);
         int opcion;

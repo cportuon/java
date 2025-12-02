914bd3d2a3b4283a722c654123885722dc5c0e85
cosme <cportuon@gmail.com>
2025-12-02 22:02:02 +0100

    exercicios conditionals

---
+++ MourePRO/src/main/java/com/mycompany/mourepro/Conditionals.java
@@ -0,0 +1,52 @@
+
+package com.mycompany.mourepro;
+import java.util.Scanner;
+
+public class Conditionals {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
+        /*
+        int edad;
+        
+        System.out.println("Introduce tu edad: ");
+        edad = sc.nextInt();
+        if (edad < 18){
+            System.out.println("Eres menor de edad, no puedes votar.");
+        }
+        else System.out.println("Eres mayor de edad, puedes votar.");*/
+        
+        // 2. Declara dos nÃºmeros y muestra cuÃ¡l es mayor, o si son iguales.
+            /*int num1;
+            int num2;
+            
+            System.out.print("Introduce un número: ");
+            num1 = sc.nextInt();
+            System.out.print("Introduce un segundo número: ");
+            num2 = sc.nextInt();
+            
+            if(num1 > num2){
+                System.out.print(num1 + " es el valor más alto.");
+            }
+            else System.out.println(num2 + " es el valor más alto.");*/
+        // 3. Dado un nÃºmero, verifica si es positivo, negativo o cero.
+
+        // 4. Crea un programa que diga si un nÃºmero es par o impar.
+
+        // 5. Verifica si un nÃºmero estÃ¡ en el rango de 1 a 100.
+
+        // 6. Declara una variable con el dÃ­a de la semana (1-7) y muestra su nombre con
+        // switch.
+
+        // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o
+        // "Suspenso" segÃºn la nota (0-100).
+
+        // 8. Escribe un programa que determine si puedes entrar al cine: debes tener al
+        // menos 15 aÃ±os o ir acompaÃ±ado.
+
+        // 9. Crea un programa que diga si una letra es vocal o consonante.
+
+        // 10. Usa tres variables a, b, c y muestra cuÃ¡l es el mayor de las tres.
+    }
+}

7c225fca8db64e4ffeec58239c9e2645a1afbacc
cosme <cportuon@gmail.com>
2025-12-06 11:48:45 +0100

    mouredev ejercicios

--- MourePRO/src/main/java/com/mycompany/mourepro/Conditionals.java
+++ MourePRO/src/main/java/com/mycompany/mourepro/Conditionals.java
@@ -31,14 +31,58 @@ public class Conditionals {
             }
             else System.out.println(num2 + " es el valor más alto.");*/
         // 3. Dado un nÃºmero, verifica si es positivo, negativo o cero.
+        /*int num;
 
+        System.out.println("Introduce un número: ");
+        num = sc.nextInt();
+        if (num < 0){
+            System.out.println("El número es negativo.");
+        }
+        else if (num > 0){
+            System.out.println("El número es positivo.");
+        }
+        else System.out.println("El número es el cero.");*/
         // 4. Crea un programa que diga si un nÃºmero es par o impar.
+        /*int num;
 
+        System.out.println("Introduce un número:");
+        num = sc.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("El número es par.");
+        }
+        else System.out.println("El número es impar.");*/
         // 5. Verifica si un nÃºmero estÃ¡ en el rango de 1 a 100.
 
         // 6. Declara una variable con el dÃ­a de la semana (1-7) y muestra su nombre con
         // switch.
-
+        /*int dia;
+        System.out.println("Introduce el número de tú día preferido de la semana: ");
+        dia = sc.nextInt();
+        switch(dia){
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default: System.out.println("Día incorrecto.");
+        }*/
         // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o
         // "Suspenso" segÃºn la nota (0-100).
 

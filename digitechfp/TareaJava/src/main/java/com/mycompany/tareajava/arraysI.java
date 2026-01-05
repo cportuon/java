0ae3ce9fe8d09cee52d020fe9712bb4644a9cd96
cosme <cportuon@gmail.com>
2026-01-05 19:39:00 +0100

    arrayI

---
+++ digitechfp/TareaJava/src/main/java/com/mycompany/tareajava/arraysI.java
@@ -0,0 +1,32 @@
+package com.mycompany.tareajava;
+import java.util.Scanner;
+
+public class arraysI {
+    public static void main(String[] arg){
+        // 1. Diseñar un array, que almacena los números del 1 al 10
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = array.length-1; i >= 0;i--){
+            System.out.println(array[i]);
+        }
+
+        /* 2.Se pide por pantalla el número de posiciones que queremos que tenga nuestro array, se
+        piden los datos por pantalla.
+        Se crea otro array, pidiendo el número de posiciones por pantalla. Los valores se cargan con un
+        bucle for.
+        Crear un tercer array que contiene, los datos de los dos arrays. */
+
+
+        /* 3. Crear un array de caracteres, con los siguientes valores: a,e,r,t,y,u. Crea un array con datos
+        enteros con valores del 1 al 6.
+        Diseña un tercer array que tiene el mismo número de celdas que la suma de las celdas de los
+        dos arrays anteriores.*/
+
+
+        /* 4.Diseña un array con los números del 1-57, se debe mostrar el número de celdas que
+        contienen números pares y el número de celdas que contienen números impares */
+
+
+        /* 5.Diseñar un array con las calificaciones de 6 asignaturas de un alumno que se piden por
+        pantalla. Se debe calcular su nota media final.*/
+    }
+}

8cfc200ace652c82c7d1041f8682c069a26e00c0
cosme <cportuon@gmail.com>
2026-01-13 19:20:46 +0100

    Ejercicio string terminado

--- digitechfp/TareaJava/src/main/java/com/mycompany/tareajava/tareaString.java
+++ digitechfp/TareaJava/src/main/java/com/mycompany/tareajava/tareaString.java
@@ -19,5 +19,57 @@ public class tareaString {
           System.out.println(password);
           System.out.println("Contraseña válida");
 
+          System.out.println("Introduce dos o tres palabras:");
+          String texto = sc.nextLine();
+
+          primeraMitad(texto);
+          ultimoCaracter(texto);
+          inversa(texto);
+          separadoGuiones(texto);
+          contarVocales(texto);
+          capicua(texto);
+
+
       }
+    public static void primeraMitad(String texto){
+        int mitad = texto.length()/2;
+        System.out.println(texto.substring(0, mitad));
+    }
+    public static void ultimoCaracter(String texto){
+          System.out.println("Último caracter es: " + texto.charAt(texto.length()-1));
+    }
+    public static void inversa(String texto){
+          String textoInvertido = "";
+          for (int i = texto.length()-1; i >= 0; i--){
+              textoInvertido += texto.charAt(i);
+          }
+        System.out.println(textoInvertido);
+    }
+
+    public static void separadoGuiones(String texto){
+          String resultado = texto.replace(" ", "-");
+          System.out.println(resultado);
+
+    }
+
+    public static void contarVocales(String texto){
+          int contador = 0;
+          for (int i = 0; i <= texto.length()-1; i++){
+              if (texto.charAt(i) == 'a' || texto.charAt(i) == 'e' || texto.charAt(i) == 'i'
+                      || texto.charAt(i) == 'o' || texto.charAt(i) == 'u'){
+                  contador++;
+              }
+          }
+          System.out.println("Contiene " + contador + " vocales.");
+    }
+    public static void capicua(String texto){
+          String isCapicua = "";
+          for (int i = texto.length()-1; i>=0;i--){
+              isCapicua += texto.charAt(i);
+          }
+          if(texto.equals(isCapicua)){
+              System.out.println("Se lee igual de derecha a izquierda que de izquierda a derecha.");
+          }
+          else System.out.println("No se lee igual de izquierda a derecha.");
+    }
 }
